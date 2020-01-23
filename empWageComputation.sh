8e6c6718d9f211d94cc0c0e5c8dc79fc99de49f1
karthik-karotra <karthikpatel54@gmail.com>
2020-01-23 12:58:41 +0530

    ADDED:Added Constants,MODIFIED:Modified Logic,DELETED:Deleted variables

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,16 +2,21 @@
 echo "Hello Employee"
 
 #CONSTANT
-IS_PRESENT=1;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
 EMPLOYEE_RATE_PER_HOUR=20;
-EMPLOYEE_HOURS=8;
 
 #VARIABLE
-randomCheck=$((RANDOM%2));
+randomCheck=$((RANDOM%3));
 
-if [ $IS_PRESENT -eq $randomCheck ];
+if [ $IS_FULL_TIME -eq $randomCheck ];
 then
-	salary=$(($EMPLOYEE_HOURS*$EMPLOYEE_RATE_PER_HOUR))
+	employeeHours=8;
+elif [ $IS_PART_TIME -eq $randomCheck ];
+then
+	employeeHours=4;
 else
-	salary=0;
+	employeeHours=0;
 fi
+
+salary=$((employeeHours*EMPLOYEE_RATE_PER_HOUR));
