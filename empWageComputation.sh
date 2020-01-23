cc34c2f50ec0e73f48b021b4caa6bb52e332f26e
karthik-karotra <karthikpatel54@gmail.com>
2020-01-23 14:32:32 +0530

    ADDED:variable added & for loop added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,24 +1,29 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 echo "Hello Employee"
 
 #CONSTANT
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 EMPLOYEE_RATE_PER_HOUR=20;
+WORKING_DAYS_PER_MONTH=20;
 
 #VARIABLE
-employeeCheck=$((RANDOM%3));
+totalSalary=0;
 
-case $employeeCheck in
-	$IS_FULL_TIME )
-		employeeHours=8
-		;;
-	$IS_PART_TIME )
-		employeeHours=4
-		;;
-	* )
-		employeeHours=0
-		;;
-esac
-
-salary=$((employeeHours*EMPLOYEE_RATE_PER_HOUR));
+for((day=1; day<=$WORKING_DAYS_PER_MONTH; day++))
+do
+	employeeCheck=$((RANDOM%3));
+	case $employeeCheck in
+		$IS_FULL_TIME )
+			employeeHours=8
+			;;
+		$IS_PART_TIME )
+			employeeHours=4
+			;;
+		* )
+			employeeHours=0
+			;;
+	esac
+	salary=$((employeeHours*EMPLOYEE_RATE_PER_HOUR))
+	totalSalary=$(($totalSalary + $salary));
+done
