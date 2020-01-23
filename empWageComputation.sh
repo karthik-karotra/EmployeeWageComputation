dbc4c31c7c7d0e8b520d6af9a6d2493a1a3aa5ac
karthik-karotra <karthikpatel54@gmail.com>
2020-01-23 15:22:43 +0530

    ADDED:Variables Added, MODIFIED:Change logic of for to while

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,12 +6,15 @@ IS_PART_TIME=1;
 IS_FULL_TIME=2;
 EMPLOYEE_RATE_PER_HOUR=20;
 WORKING_DAYS_PER_MONTH=20;
+MAX_HOURS_IN_MONTH=100;
 
 #VARIABLE
-totalSalary=0;
+totalEmployeeHours=0;
+totalWorkingDays=0;
 
-for((day=1; day<=$WORKING_DAYS_PER_MONTH; day++))
+while [[ $totalEmployeeHours -lt $MAX_HOURS_IN_MONTH && $totalWorkingDays -lt $WORKING_DAYS_PER_MONTH ]]
 do
+	((totalWorkingDays++))
 	employeeCheck=$((RANDOM%3));
 	case $employeeCheck in
 		$IS_FULL_TIME )
@@ -24,6 +27,6 @@ do
 			employeeHours=0
 			;;
 	esac
-	salary=$((employeeHours*EMPLOYEE_RATE_PER_HOUR))
-	totalSalary=$(($totalSalary + $salary));
+	totalEmployeeHours=$(($totalEmployeeHours + $employeeHours))
 done
+	totalSalary=$(($totalEmployeeHours*$EMPLOYEE_RATE_PER_HOUR));
