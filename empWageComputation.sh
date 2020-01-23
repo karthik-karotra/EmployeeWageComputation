099205ca9fb17d9b26dd0bc689fe552924a8dcc1
karthik-karotra <karthikpatel54@gmail.com>
2020-01-23 11:53:14 +0530

    ADDED: Calculating Daily Emp Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,13 +3,15 @@ echo "Hello Employee"
 
 #CONSTANT
 IS_PRESENT=1;
+EMP_RATE_PER_HR=20;
+EMP_HRS=8;
 
 #VARIABLE
 randomCheck=$((RANDOM%2));
 
 if [ $IS_PRESENT -eq $randomCheck ];
 then
-	echo "Employee is Present" 
+	salary=$(($EMP_HRS*$EMP_RATE_PER_HR))
 else
-	echo "Employee is Absent"
+	salary=0;
 fi
